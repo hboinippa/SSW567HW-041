a1dc55557761bbb1c5e9362dffb5c23c58bf8fa3
hboinippa <hboinipa@stevens.edu>
2022-10-07 18:01:00 -0400

    before final commit

--- gitapitest.py
+++ gitapitest.py
@@ -6,7 +6,7 @@ class Testgithubapi(unittest.TestCase):
     def testgithub(self):
         self.assertEqual(githubapi('?'), False)
     def testgithub2(self):
-        self.assertEqual(githubapi('hboinippa'), False)
+        self.assertEqual(githubapi('hboinip'), False)
     def testgithub3(self):
         self.assertEqual(githubapi('SSW567HW-041'), False)
     def testgithub4(self):

ddbe69259a84e5bb7afc2cd8a892e7d212e7f7da
hboinippa <hboinipa@stevens.edu>
2022-10-12 23:05:05 -0400

    first commit

---
+++ hw05a.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+
+def getUserRepos(userName):       
+    API = ("https://api.github.com/users/" + userName + "/repos")
+    userData = requests.get(API)
+    repositories = json.loads(userData.text)
+    userRepos = []
+    
+    for repository in repositories:
+        try:
+            userRepos.append(repository.get("name"))
+        except:
+            userRepos = []
+         
+    return userRepos
+
+def getCommitnum(userName, repoName):          #  number of commits that are in each of the listed repositories.
+    API = "https://api.github.com/repos/" + userName + "/" + repoName + "/commits"
+    repoData = requests.get(API)
+    commits = json.loads(repoData.text)
+    Commitnum = len(commits)
+
+    return Commitnum
+        
+
+if __name_ == "_main_":
+    userName = input("Enter Github username: ")     
+    userRepos = getUserRepos(userName) 
+    print("User: " + userName)
+    for repository in userRepos:              #Use for Loop to printing name and their commit in associated repos 
+        Commitnum = getCommitnum(userName, repository)
+        print("Repo: " + repository + " Number of Commits: " + str(Commitnum))
